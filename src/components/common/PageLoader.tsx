493d7b79359f3f49db42d4897a0069bd547d3b13
khademmh <khadem5939@gmail.com>
2026-03-08 11:17:21 +0330

    Redesign PageLoader

--- src/components/common/PageLoader.tsx
+++ src/components/common/PageLoader.tsx
@@ -5,17 +5,17 @@ import LoaderDots from "./LoaderDots";
 
 export default function PageLoader() {
   return (
-    <div className="w-screen bg-black/20 h-screen z-[99999] flex items-center justify-center fixed top-0 left-0">
-      <Card className="w-full max-w-xs">
-        <CardContent className="w-full flex flex-col items-center gap-10">
+    <div className="w-screen bg-black/50 h-screen z-[99999] flex items-center justify-center fixed inset-0">
+      <Card className="w-full max-w-xs rounded-xl h-[185px]">
+        <CardContent className="w-full h-full flex flex-col items-center justify-center gap-6">
           <Image
             priority
-            width={120}
-            height={120}
+            width={125}
+            height={125}
             src={"/rshop_logo_h.png"}
             alt="logo"
           />
-          <LoaderDots count={3} className="text-primary-400" size={10} />
+          <LoaderDots count={3} className="text-primary-400" size={11} />
         </CardContent>
       </Card>
     </div>
