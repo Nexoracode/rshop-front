9cf6ecbe50593f126ebd10c4b00f28131744bda6
khademmh <khadem5939@gmail.com>
2026-03-12 13:50:27 +0330

    Update

--- src/components/domain/checkout/CheckoutForm.tsx
+++ src/components/domain/checkout/CheckoutForm.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import DiscountField from "./DiscountForm";
 import AddressSelector from "./AddressSelector";
@@ -12,10 +14,23 @@ import { Card } from "@/components/ui/card";
 import Responsive from "@/components/common/Responsive";
 import SectionTitle from "@/components/shared/asset/SectionTitle";
 import UpdateUserInfo from "./UpdateUserInfo";
+import Image from "@/components/common/Image";
+import BackButton from "@/components/common/BackButton";
+import { useRouter } from "next/navigation";
 
 export default function CheckoutForm() {
+  const router = useRouter()
   return (
     <div className="min-h-screen space-y-5">
+      <div className="relative border border-slate-300 rounded-lg p-3 mb-16">
+        <div onClick={() => router.back()} className="absolute cursor-pointer flex items-center gap-2 top-10 -translate-y-1/2 right-6">
+          <BackButton />
+            برگشت
+        </div>
+        <div className="w-full flex items-center justify-center">
+          <Image src={"/rshop_logo_h.png"} width={100} height={45} alt="logo" />
+        </div>
+      </div>
       <QueryClientWrapper>
         <SectionTitle title="تکمیل سفارش" />
         <div className="flex items-start gap-5">
