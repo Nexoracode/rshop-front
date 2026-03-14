f6a83ecbe631a7a5d0fbd4afa0658a5e76922d03
khademmh <khadem5939@gmail.com>
2026-03-14 13:11:35 +0330

    Update

--- src/components/common/EmptyState.tsx
+++ src/components/common/EmptyState.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FileX } from "lucide-react";
+import { LucideBox } from "lucide-react";
 
 export default function EmptyState({
   title = "هیچ موردی یافت نشد",
@@ -11,9 +11,12 @@ export default function EmptyState({
 }) {
   return (
     <div className="flex flex-col w-full items-center justify-center col-span-12 gap-2 py-10 text-center text-muted-foreground">
-      <FileX className="w-20 h-20 opacity-30" />
-      <h3 className="font-medium text-lg text-muted/80">{title}</h3>
-      {description && <p className="text-sm text-muted/50">{description}</p>}
+      <div className="relative w-fit">
+        <div className="absolute left-5 w-20 h-20 bg-green-200 rounded-full"></div>
+        <LucideBox className="w-24 h-24 opacity-30 mx-auto" />
+        <h3 className="font-medium text-lg text-muted/80">{title}</h3>
+        {description && <p className="text-sm text-muted/50">{description}</p>}
+      </div>
     </div>
   );
 }
