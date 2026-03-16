7ab669fbadc70e15e6ddf569ba66efc9cb982947
khademmh <khadem5939@gmail.com>
2026-03-16 09:33:35 +0330

    Update ProductSummeryCard

--- src/components/domain/Product/ProductSummeryCard.tsx
+++ src/components/domain/Product/ProductSummeryCard.tsx
@@ -1,15 +1,14 @@
 "use client";
+
 import { Product } from "@/types/product";
 import React from "react";
 import { Card } from "../../ui/card";
 import Image from "../../common/Image";
 import { PRODUCT_PLACEHOLDER } from "@/data/assets";
 import AddToCartButton from "./AddToCart/AddToCartButton";
-import { Separator } from "../../ui/separator";
 import ProductPriceInfo from "./ProductInfo/ProductPriceInfo";
 import usePromotionPadding from "@/hooks/usePromotionPadding";
 import VariantValues from "./VariantValues";
-import { ShoppingBagIcon, StoreIcon, ZapIcon } from "lucide-react";
 import { cn } from "@/lib/utils/classnames";
 import useProductVariantUrl from "@/hooks/useProductVariantUrl";
 
@@ -22,7 +21,7 @@ export default function ProductSummeryCard(product: Product) {
   return (
     <Card
       className={cn(
-        "hidden space-y-4 md:block w-[300px] sticky top-[5rem] h-fit",
+        "hidden space-y-4 md:block w-[300px] sticky top-2 mt-14 h-fit",
         bannerExists && "top-[10rem]",
       )}
     >
@@ -42,29 +41,12 @@ export default function ProductSummeryCard(product: Product) {
           {variant && <VariantValues variant={variant} />}
         </div>
       </div>
-      <Separator />
 
-      <ProductPriceInfo {...product} />
-
-      <Separator />
-
-      <AddToCartButton product={product} />
-
-      <div>
-        <div className="flex items-center text-sm  text-primary py-1 gap-2">
-          <StoreIcon className="text-primary size-5" />
-          آرشاب
+      <AddToCartButton product={product}>
+        <div className="my-2">
+          <ProductPriceInfo {...product} />
         </div>
-        <div className="flex items-center text-muted-light text-xs py-1 gap-2">
-          <ShoppingBagIcon className="text-black size-4" />
-          موجود در انبار آرشاب
-        </div>
-
-        <div className="flex items-center text-muted-light text-xs py-1 gap-2">
-          <ZapIcon className="text-secondary size-4" />
-          ارسال در سریع ترین زمان
-        </div>
-      </div>
+      </AddToCartButton>
     </Card>
   );
 }
