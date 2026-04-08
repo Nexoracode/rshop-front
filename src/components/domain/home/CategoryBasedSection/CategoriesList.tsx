0aff9e2c34f3e33c976b90412d486d7395d768a5
khademmh <khadem5939@gmail.com>
2026-04-08 12:28:07 +0330

    Update CategoriesList

--- src/components/domain/home/CategoryBasedSection/CategoriesList.tsx
+++ src/components/domain/home/CategoryBasedSection/CategoriesList.tsx
@@ -19,16 +19,16 @@ export default function CategoriesList({
       className="overflow-scroll no-scrollbar max-h-[226px] h-full"
       dir="rtl"
     >
-      <div className="flex min-w-fit md:w-[10rem] md:flex-col gap-2 flex-nowrap">
+      <div className="flex min-w-fit md:w-[10rem] md:flex-col gap-1 flex-nowrap">
         {sections.map(({ category, id }) => (
           <div
             key={id}
             role="button"
             onClick={() => onSelect(id)}
             className={cn(
-              "flex flex-col md:flex-row min-w-fit md:w-full px-2 md:flex-1 items-center gap-1 p-1.5 cursor-pointer transition-colors rounded-lg overflow-hidden",
+              "flex flex-col md:flex-row min-w-fit md:w-full px-2 rounded-lg md:flex-1 items-center gap-1 p-1.5 cursor-pointer transition-colors overflow-hidden",
               "",
-              selected === id && "bg-primary-300 rounded-lg",
+              selected === id && "bg-primary-300",
             )}
           >
             <Image
@@ -36,10 +36,10 @@ export default function CategoriesList({
               width={48}
               height={48}
               alt={category.name}
-              className={`w-14 h-14 md:w-12 md:h-12 border border-white rounded-full`}
+              className={`w-14 h-14 md:w-12 md:h-12 rounded-full`}
             />
 
-            <span className={`text-xs font-bold mt-1 md:mt-0 md:text-[15px] ${selected === id ? "text-black" : "text-white"} pr-2`}>
+            <span className={`text-xs font-bold mt-1 md:mt-0 md:text-sm text-white pr-2`}>
               {category.name}
             </span>
           </div>
