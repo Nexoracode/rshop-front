20a162cc8413a47a61d330bb45998c90c33e69ac
khademmh <khadem5939@gmail.com>
2026-04-11 09:26:12 +0330

    Test

--- src/components/domain/home/HomeSectionItem/ProductGridCarousel.tsx
+++ src/components/domain/home/HomeSectionItem/ProductGridCarousel.tsx
@@ -15,20 +15,25 @@ type Props = {
 
 export default function ProductGridCarousel({ products }: Props) {
   const chunckedProducts = chunkArray(products, 4);
+
   return (
     <Carousel>
       <CarouselContent>
         {chunckedProducts.map((group) => (
           <CarouselItem key={group[0].id} className="basis-[95%]">
             <div className="grid grid-cols-2">
-              {group.map((product) => (
-                <div
-                  key={product.id}
-                  className="border-b border-l nth-[3]:border-b-0"
-                >
-                  <ProductCartItem {...product} />
-                </div>
-              ))}
+              {group.map((product, index) => {
+                const isSecondRowInCarouselItem = index >= 2;
+
+                return (
+                  <div
+                    key={product.id}
+                    className={`border-l ${isSecondRowInCarouselItem ? "border-b-0" : "border-b-1"}`}
+                  >
+                    <ProductCartItem {...product} />
+                  </div>
+                );
+              })}
             </div>
           </CarouselItem>
         ))}
