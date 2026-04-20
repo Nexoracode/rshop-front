5bf17cd280f3f8d0c198e260eb5420838efffe3c
khademmh <khadem5939@gmail.com>
2026-04-20 13:29:16 +0330

    Updater

--- src/components/domain/cart/CartItemsList.tsx
+++ src/components/domain/cart/CartItemsList.tsx
@@ -13,24 +13,23 @@ export default function CartItemsList() {
     return (
       <div className="flex gap-5">
         <div className="w-full flex flex-col gap-4">
-          <div className="flex flex-col justify-center border border-slate-200 rounded-lg">
-            <div className="w-full h-[126px] flex p-4">
-              <div className="w-[100px] h-[95px] rounded-lg bg-slate-100 animate-pulse"></div>
+          <div className="flex flex-col justify-center sm:border border-slate-200 rounded-lg">
+            <div className="w-full h-[126px] flex items-end p-2 sm:p-4">
+              <div className="min-w-[100px] h-[95px] rounded-lg bg-slate-100 animate-pulse"></div>
               <div className="w-full h-full flex items-end justify-end">
                 <div className="w-[90px] h-[30px] rounded-md bg-slate-100 animate-pulse"></div>
               </div>
             </div>
-            <div className="border-b border-slate-100 mx-4"></div>
-            <div className="w-full h-[126px] rounded-lg flex p-4">
-              <div className="w-[100px] h-[95px] rounded-lg bg-slate-100 animate-pulse"></div>
+            <div className="border-b border-slate-100 mx-2 mt-4 sm:mt-0 sm:mx-4"></div>
+            <div className="w-full h-[126px] rounded-lg flex items-end p-2 sm:p-4">
+              <div className="min-w-[100px] h-[95px] rounded-lg bg-slate-100 animate-pulse"></div>
               <div className="w-full h-full flex items-end justify-end">
                 <div className="w-[90px] h-[30px] rounded-md bg-slate-100 animate-pulse"></div>
               </div>
             </div>
           </div>
         </div>
-        <div className="border border-slate-200 min-w-[350px] h-[314px] rounded-lg p-5 hidden lg:flex flex-col gap-4 justify-between">
-          <div className="bg-slate-100 w-full h-full rounded-lg animate-pulse"></div>
+        <div className="border border-slate-200 min-w-[350px] h-[314px] rounded-lg p-5 hidden lg:flex flex-col gap-4 justify-end">
           <div className="w-full bg-slate-100 !min-h-[48px] rounded-lg animate-pulse"></div>
         </div>
       </div>
--- src/components/domain/cart/RecentView.tsx
+++ src/components/domain/cart/RecentView.tsx
@@ -18,18 +18,18 @@ export default function RecentView() {
 
   if (!data?.length || isPending)
     return (
-      <div className="border border-slate-200 w-full h-[345px] rounded-lg lg:mt-16 p-6 pb-0 pr-0 flex flex-col justify-between gap-4">
+      <div className="sm:border border-slate-200 w-full h-[345px] rounded-lg lg:mt-16 sm:p-6 pb-0 pr-0 flex flex-col justify-around sm:justify-between gap-4">
         <p className="text-[20px] mt-2 mr-6 bg-slate-100 h-[30px] w-46 rounded-md animate-pulse"></p>
         <div className="w-full h-[250px] flex items-center pr-2">
           <div className="w-[180px] h-full border-l-2 border-slate-200 animate-pulse rounded-r-lg flex flex-col justify-between p-4">
             <div className="bg-slate-100 rounded-md w-full h-[130px]"></div>
             <div className="bg-slate-100 rounded-md w-full h-[30px]"></div>
           </div>
-          <div className="w-[180px] h-full border-l-2 border-slate-200 animate-pulse flex flex-col justify-between p-4">
+          <div className="w-[180px] h-full min-[576px]:border-l-2 border-slate-200 animate-pulse flex flex-col justify-between p-4">
             <div className="bg-slate-100 rounded-md w-full h-[130px]"></div>
             <div className="bg-slate-100 rounded-md w-full h-[30px]"></div>
           </div>
-          <div className="w-[180px] h-full animate-pulse p-4 flex flex-col justify-between gap-4">
+          <div className="w-[180px] h-full animate-pulse p-4 hidden min-[576px]:!flex flex-col justify-between gap-4">
             <div className="bg-slate-100 rounded-md w-full h-[130px]"></div>
             <div className="bg-slate-100 rounded-md w-full h-[30px]"></div>
           </div>
@@ -53,7 +53,7 @@ export default function RecentView() {
           {products.map((product, index: number) => (
             <CarouselItem
               key={index}
-              className="border-l last:border-l-0 basis-[11rem] sm:basis-[12rem]"
+              className="border-l last:border-l-0 basis-[11rem] sm:basis-[12rem] !pr-0"
             >
               <RecentViewedCard product={product} />
             </CarouselItem>
