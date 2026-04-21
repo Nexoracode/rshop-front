dfd64606aa1911ea98d7f1392da53f2d54903da7
khademmh <khadem5939@gmail.com>
2026-04-21 12:55:02 +0330

    Redesign CartItem

--- src/components/domain/cart/CartItem.tsx
+++ src/components/domain/cart/CartItem.tsx
@@ -28,16 +28,16 @@ export default function CartItem({
   return (
     <div
       key={id}
-      className="border-b pb-5 last:pb-0 border-slate-200 last:border-0 flex flex-col justify-between items-stretch"
+      className="border-b pb-5 last:pb-0 border-slate-200 last:border-0"
     >
-      <div className="w-full flex justify-between items-end gap-4">
-        <div className="flex items-center gap-4">
+      <div className="w-full flex flex-col sm:flex-row justify-between items-end gap-4">
+        <div className="w-full sm:w-fit flex items-center gap-4">
           <Image
             src={product.media_pinned?.url || PRODUCT_PLACEHOLDER}
             alt={product.name}
             width={100}
             height={100}
-            className="h-[100px] object-cover rounded-lg"
+            className="h-[100px] border sm:border-0 object-cover rounded-lg"
           />
           <div className="flex flex-col gap-3.5">
             <div className="flex items-center justify-between">
@@ -48,32 +48,27 @@ export default function CartItem({
           </div>
         </div>
 
-        <div className="flex flex-col justify-between py-1">
-          <div className="w-full flex items-center justify-between">
-            <div className="flex gap-8">
-              <div className="flex flex-col justify-end items-end">
-                {+discount > 0 && (
-                  <PriceBox
-                    className="font-medium text-[13px] text-green-600 mb-1"
-                    suffix="تومان تخفیف"
-                    price={+discount * quantity}
-                    iconClass="size-5.5"
-                    showToman={false}
-                  />
-                )}
-                <PriceBox className="font-medium" price={+line_total} />
-              </div>
-              <QuantitySelect
-                maxQty={product.stock}
-                onChange={handleQtyChange}
-                qty={quantity}
-                loading={deletePending || updatePending}
+        <div className="flex w-full sm:w-fit sm:flex-col sm:h-[100px] sm:pt-1.5 md:pt-0 md:flex-row gap-8 items-end md:items-center justify-between sm:justify-center">
+          <div className="flex flex-col justify-end sm:items-end">
+            {+discount > 0 && (
+              <PriceBox
+                className="font-medium text-[13px] text-green-600 mb-1"
+                suffix="تومان تخفیف"
+                price={+discount * quantity}
+                iconClass="size-5.5"
+                showToman={false}
               />
-            </div>
+            )}
+            <PriceBox className="font-medium" price={+line_total} />
           </div>
+          <QuantitySelect
+            maxQty={product.stock}
+            onChange={handleQtyChange}
+            qty={quantity}
+            loading={deletePending || updatePending}
+          />
         </div>
       </div>
-
     </div>
   );
 }
