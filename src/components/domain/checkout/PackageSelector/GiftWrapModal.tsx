6cfdec5bec4543c1ee1b3ccac8320872549e83de
khademmh <khadem5939@gmail.com>
2026-04-23 13:02:41 +0330

    Update

--- src/components/domain/checkout/PackageSelector/GiftWrapModal.tsx
+++ src/components/domain/checkout/PackageSelector/GiftWrapModal.tsx
@@ -2,27 +2,27 @@
 
 import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import BaseDialog from "@/components/common/BaseDialog";
 import useCheckout from "@/hooks/useCheckout";
-import { Check, Edit2Icon, LucidePlus } from "lucide-react";
+import { Check, Edit2Icon, LucidePlus, Trash2Icon } from "lucide-react";
 import { getGiftWrappings } from "@/queries/checkout/order-meta";
 import { cn } from "@/lib/utils/classnames";
 import { formatToman } from "@/lib/utils/price";
+import { SelectedGiftWrapCard } from "./SelectedGiftWrapCard";
 
 type Props = {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
   is_gift: boolean;
   onHandleSelectGift: (val: boolean) => void;
 };
 
 export function GiftWrapModal({
-  onOpenChange,
-  open,
   is_gift,
   onHandleSelectGift,
 }: Props) {
+  
+  const [open, setOpen] = useState(false)
+
   const { data } = useQuery(getGiftWrappings);
   const {
     orderMeta: { gift_wrapping_id },
@@ -32,77 +32,115 @@ export function GiftWrapModal({
   const handleSelectPack = (selectedId: number) => {
     handleSetOrderMeta({ gift_wrapping_id: selectedId });
     onHandleSelectGift(true);
-    onOpenChange(false);
+    setOpen(false);
+  };
+
+  const handleSelectGift = (is_gift: boolean) => {
+    if (is_gift) {
+      handleSetOrderMeta({ is_gift });
+    }
+  };
+
+  const onDelete = () => {
+    handleSetOrderMeta({
+      gift_wrapping_id: undefined,
+      gift_message: "",
+      is_gift: false,
+    });
   };
 
   const handleClose = () => {
     if (!gift_wrapping_id) {
       handleSetOrderMeta({ is_gift: false });
     }
-    onOpenChange(false);
+    setOpen(false);
+  };
+
+  const GiftModal = () => {
+    return (
+      <BaseDialog
+        title={is_gift ? "ویرایش انتخاب" : "انتخاب بسته بندی"}
+        open={open}
+        onOpenChange={setOpen}
+        content={
+          <div className="relative">
+            <div className="md:max-h-[70vh] pb-16 overflow-auto no-scrollbar space-y-2">
+              {data
+                ?.filter((i) => i.is_active && i.is_for_gift)
+                .map((item, index) => (
+                  <div
+                    role="button"
+                    key={index}
+                    onClick={() => handleSelectPack(item.id)}
+                    className={cn(
+                      "border relative flex items-stretch cursor-pointer rounded-lg overflow-hidden hover:border-muted-500 transition",
+                      gift_wrapping_id === item.id &&
+                        "border-green-500 bg-green-500/10",
+                    )}
+                  >
+                    <div className="relative aspect-square w-[7rem] bg-blue-50 p-2">
+                      <Image
+                        src={item.image?.url || "/gift.png"}
+                        alt={item.name}
+                        fill
+                        className="object-contain rounded-xl p-3"
+                      />
+                    </div>
+
+                    <div className="p-3 flex-1 flex-col flex justify-between text-right">
+                      <div className="font-medium text-muted">{item.name}</div>
+                      <div className="text-xs text-muted/70">
+                        {item.description}
+                      </div>
+                      <div className="text-sm font-medium text-gray-600">
+                        {formatToman(Number(item.price))}
+                      </div>
+                    </div>
+
+                    {gift_wrapping_id === item.id && (
+                      <Check className="absolute left-3 top-3 text-green-600" />
+                    )}
+                  </div>
+                ))}
+            </div>
+          </div>
+        }
+        trigger={
+          <div className="flex items-center gap-1 text-primary-500 cursor-pointer hover:text-primary-600 transition-all">
+            {!is_gift ? (
+              <LucidePlus className="size-4" />
+            ) : (
+              <Edit2Icon className="size-4" />
+            )}
+            <span className="font-medium text-sm">
+              {!is_gift ? "افزودن" : ""}
+            </span>
+          </div>
+        }
+        hiddenFooter
+      />
+    );
   };
 
   return (
-    <BaseDialog
-      title={is_gift ? "ویرایش انتخاب" : "انتخاب بسته بندی"}
-      open={open}
-      onOpenChange={onOpenChange}
-      content={
-        <div className="relative">
-          <div className="md:max-h-[70vh] pb-16 overflow-auto no-scrollbar space-y-2">
-            {data
-              ?.filter((i) => i.is_active && i.is_for_gift)
-              .map((item, index) => (
-                <div
-                  role="button"
-                  key={index}
-                  onClick={() => handleSelectPack(item.id)}
-                  className={cn(
-                    "border relative flex items-stretch cursor-pointer rounded-lg overflow-hidden hover:border-muted-500 transition",
-                    gift_wrapping_id === item.id &&
-                      "border-green-500 bg-green-500/10",
-                  )}
-                >
-                  <div className="relative aspect-square w-[7rem] bg-blue-50 p-2">
-                    <Image
-                      src={item.image?.url || "/gift.png"}
-                      alt={item.name}
-                      fill
-                      className="object-contain rounded-xl p-3"
-                    />
-                  </div>
+    <div className="gap-2 items-center">
+      <div className="flex items-center justify-between gap-8 mb-2">
+        <div className="text-[13px] text-muted-light">بسته بندی:</div>
 
-                  <div className="p-3 flex-1 flex-col flex justify-between text-right">
-                    <div className="font-medium text-muted">{item.name}</div>
-                    <div className="text-xs text-muted/70">
-                      {item.description}
-                    </div>
-                    <div className="text-sm font-medium text-gray-600">
-                      {formatToman(Number(item.price))}
-                    </div>
-                  </div>
-
-                  {gift_wrapping_id === item.id && (
-                    <Check className="absolute left-3 top-3 text-green-600" />
-                  )}
-                </div>
-              ))}
+        {is_gift ? (
+          <div className="flex items-center gap-3">
+            {GiftModal()}
+            <div className="flex items-center gap-1 text-slate-500 cursor-pointer hover:text-red-500 transition-all">
+              <Trash2Icon className="size-4" onClick={onDelete} />
+            </div>
           </div>
-        </div>
-      }
-      trigger={
-        <div className="flex items-center gap-1 text-primary-500 cursor-pointer hover:text-primary-600 transition-all">
-          {!is_gift ? (
-            <LucidePlus className="size-4" />
-          ) : (
-            <Edit2Icon className="size-4" />
-          )}
-          <span className="font-medium text-sm">
-            {!is_gift ? "افزودن" : ""}
-          </span>
-        </div>
-      }
-      hiddenFooter
-    />
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="font-medium text-sm text-muted">
+        {is_gift ? <SelectedGiftWrapCard /> : GiftModal()}
+      </div>
+    </div>
   );
 }
--- src/components/domain/checkout/PackageSelector/index.tsx
+++ src/components/domain/checkout/PackageSelector/index.tsx
@@ -1,76 +1,28 @@
 "use client";
 
-import { useState } from "react";
-import { Trash2Icon } from "lucide-react";
-import { SelectedGiftWrapCard } from "./SelectedGiftWrapCard";
 import GiftMessageInput from "./GiftMessageInput";
-import { GiftWrapModal } from "./GiftWrapModal";
 import useCheckout from "@/hooks/useCheckout";
 import { useQuery } from "@tanstack/react-query";
 import { getGiftWrappings } from "@/queries/checkout/order-meta";
+import { GiftWrapModal } from "./GiftWrapModal";
 
 export function PackageSelector() {
-  const [open, setOpen] = useState(false);
-
   const {
     orderMeta: { is_gift = false, gift_wrapping_id },
     handleSetOrderMeta,
   } = useCheckout();
-
   const { data } = useQuery(getGiftWrappings);
 
-  if (data && data.length === 0) return null;
+  if (!data?.length) return null;
 
-  const giftModal = () => {
-    return (
-      <GiftWrapModal
-        open={open}
-        onOpenChange={setOpen}
-        is_gift={is_gift}
-        onHandleSelectGift={(val) => handleSelectGift(val)}
-      />
-    );
-  };
-
-  const handleSelectGift = (is_gift: boolean) => {
-    if (is_gift) {
-      handleSetOrderMeta({ is_gift });
-    }
-  };
-
-  const onDelete = () => {
-    handleSetOrderMeta({
-      gift_wrapping_id: undefined,
-      gift_message: "",
-      is_gift: false,
-    });
-  };
+  console.log(is_gift, gift_wrapping_id);
 
   return (
     <>
-      {/* setGiftModal(true) edit*/}
-      {/*onClick={() => handleSetOrderMeta({ note: "" })}  */}
-
-      <div className="gap-2 items-center">
-        <div className="flex items-center justify-between gap-8 mb-2">
-          <div className="text-[13px] text-muted-light">بسته بندی:</div>
-
-          {is_gift ? (
-            <div className="flex items-center gap-3">
-              {giftModal()}
-              <div className="flex items-center gap-1 text-slate-500 cursor-pointer hover:text-red-500 transition-all">
-                <Trash2Icon className="size-4" onClick={onDelete} />
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="font-medium text-sm text-muted">
-          {is_gift ? <SelectedGiftWrapCard /> : giftModal()}
-        </div>
-      </div>
-
+      <GiftWrapModal
+        is_gift={is_gift}
+        onHandleSelectGift={(giftID) => console.log(giftID)}
+      />
       {is_gift ? <GiftMessageInput /> : ""}
     </>
   );
