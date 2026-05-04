da445d8a67be7b11a16d1ec9491a703d89b58ac7
khademmh <khadem5939@gmail.com>
2026-05-04 13:57:51 +0330

    Update Footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -21,7 +21,10 @@ export default function Footer() {
       links: [
         { label: "پربازدیدترین ها", href: "/products?sortBy=visited" },
         { label: "پرفروش‌‌ها", href: "/products?sortBy=bestselling" },
-        { label: "تخفیف دارها", href: "/products?query=filter%5Bspecial_offer%5D=1" },
+        {
+          label: "تخفیف دارها",
+          href: "/products?query=filter%5Bspecial_offer%5D=1",
+        },
         { label: "محبوب ترین ها", href: "/products?sortBy=popular" },
       ],
     },
@@ -35,31 +38,41 @@ export default function Footer() {
   ];
 
   return (
-    <footer className={"lg:border-t bg-white relative z-20 border-slate-200 lg:mt-24 pt-7 pb-20 md:pb-0"}>
-      <div className={"max-w-[87rem] mx-auto px-4"}>
-        <div className="flex items-center justify-center mb-8 lg:mb-12 lg:justify-between">
-          <BrandLogo />
+    <footer
+      className={
+        "bg-white relative z-20 pt-7 lg:!pt-24 pb-20 md:pb-0"
+      }
+    >
+      <div className="lg:pt-7 lg:border-t border-slate-200">
+        <div className={"max-w-[87rem] mx-auto px-4 "}>
+          <div className="flex items-center justify-center mb-8 lg:mb-12 lg:justify-between">
+            <BrandLogo />
 
-          <BackToTopButton />
-        </div>
+            <BackToTopButton />
+          </div>
 
-        <ServiceSection />
+          <ServiceSection />
 
-        <div className="grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-4">
-          {linkColumns.map((col) => (
-            <FooterColumn key={col.title} title={col.title} links={col.links} />
-          ))}
-          <div className="space-y-6">
-            <button className="text-sm md:text-[17px] flex items-center justify-between w-full font-medium">
-              ارتباط با ما
-            </button>
-            <div className="flex flex-col gap-5">
-              <ContactSection />
-              <SocialSection />
+          <div className="grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-4">
+            {linkColumns.map((col) => (
+              <FooterColumn
+                key={col.title}
+                title={col.title}
+                links={col.links}
+              />
+            ))}
+            <div className="space-y-6">
+              <button className="text-sm md:text-[17px] flex items-center justify-between w-full font-medium">
+                ارتباط با ما
+              </button>
+              <div className="flex flex-col gap-5">
+                <ContactSection />
+                <SocialSection />
+              </div>
             </div>
           </div>
+          <CopyRightSection />
         </div>
-        <CopyRightSection />
       </div>
     </footer>
   );
