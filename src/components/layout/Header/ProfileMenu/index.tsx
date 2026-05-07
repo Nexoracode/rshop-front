1fae2e57cbc3bbcb7cf43da1bb09414636c24508
khademmh <khadem5939@gmail.com>
2026-05-07 13:47:12 +0330

    resolve problem

--- src/components/layout/Header/ProfileMenu/index.tsx
+++ src/components/layout/Header/ProfileMenu/index.tsx
@@ -16,6 +16,7 @@ import {
   User2Icon,
   Eye,
   LogOut,
+  MapIcon,
 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -23,9 +24,7 @@ import React from "react";
 import LogoutButton from "../../../domain/profile/LogoutButton";
 import { Skeleton } from "@/components/ui/skeleton";
 import useCurrentUser from "@/hooks/useCurrentUser";
-<<<<<<< HEAD
 import UserMenuItem from "./UserMenuItem"; // masire dorost ro vared kon
-=======
 import useCart from "@/hooks/useCart";
 
 const menuItems = [
@@ -34,16 +33,14 @@ const menuItems = [
   { label: "علاقه مندی ها", Icon: Heart, href: "wishlist" },
   { label: "دیدگاه ها", Icon: MessageCircle, href: "reviews" },
 ];
->>>>>>> a807796e2ccd8e090aa0af8eb51f8dc5830c0cbb
 
 export default function ProfileMenu() {
   const { user, isPending } = useCurrentUser();
   const { isPending: cartPending } = useCart();
   const path = usePathname();
-<<<<<<< HEAD
 
-  if (isPending) {
-    return <Skeleton className="h-8 w-33" />;
+  if (isPending || cartPending) {
+    return <Skeleton className="h-8 w-24" />;
   }
 
   if (!user) {
@@ -116,75 +113,5 @@ export default function ProfileMenu() {
         </ul>
       </PopoverContent>
     </Popover>
-=======
-  return isPending || cartPending ? (
-    <Skeleton className="h-8 w-22" />
-  ) : (
-    <>
-      {user ? (
-        <>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="text-nohover"
-                className="!flex items-center px-0 text-slate-700"
-                endIcon={<ChevronDownIcon size={15} />}
-              >
-                <User2Icon size={24} className="text-slate-700" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="p-0 rounded-lg" align="end">
-              <div className="p-4">
-                <Link
-                  href="/profile"
-                  className="flex items-center justify-between gap-1.5 mb-4"
-                >
-                  <div className="flex items-center gap-2">
-                    <span className="!w-10 !h-10 rounded-lg bg-slate-100 content-center">
-                      <UserIcon size={24} className="text-slate-600" />
-                    </span>
-                    <span className="w-42 truncate text-sm text-slate-800">
-                      {user.first_name ? (
-                        <span>{`${user.first_name} ${user.last_name}`}</span>
-                      ) : (
-                        <span className="text-sm text-muted">{user.phone}</span>
-                      )}
-                    </span>
-                  </div>
-                  <ChevronLeft size={19} className="text-slate-700" />
-                </Link>
-                {menuItems.map(({ Icon, href, label }) => (
-                  <UserMenuItem
-                    key={href}
-                    Icon={<Icon size={19} />}
-                    label={label}
-                    href={`/profile/${href}`}
-                  />
-                ))}
-
-                <div className="pr-2">
-                  <LogoutButton />
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </>
-      ) : (
-        <Button
-          href={{ pathname: "/users/login", query: { backUrl: path } }}
-          variant={"outline"}
-          color="neutral"
-          className="text-black border h-[40px] rounded-md border-slate-300"
-          size={"sm"}
-          aria-label="ورود"
-          startIcon={
-            <LogInIcon className="-scale-x-100 text-gray-700" size={22} />
-          }
-        >
-          <span className="inline-block text-[13px]">ورود | ثبت نام</span>
-        </Button>
-      )}
-    </>
->>>>>>> a807796e2ccd8e090aa0af8eb51f8dc5830c0cbb
-  );
+  )
 }
