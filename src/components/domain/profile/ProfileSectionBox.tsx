23400bdc65073d02c1e24782fa0c9fdc7f91a82b
khademmh <khadem5939@gmail.com>
2026-05-07 13:33:14 +0330

    Test

--- src/components/domain/profile/ProfileSectionBox.tsx
+++ src/components/domain/profile/ProfileSectionBox.tsx
@@ -13,12 +13,12 @@ const ProfileSectionBox = ({
     <div
       className={`min-w-full min-h-[348px] lg:border rounded-lg lg:p-5 ${className}`}
     >
-      <div className="flex justify-between items-center mb-12">
+      <div className="flex justify-between items-center mb-6">
         <div className="relative flex items-center justify-between">
           <h1 className="text-base md:text-[17px] font-medium flex items-center gap-2">
             {title}
           </h1>
-          <div className="absolute -bottom-2.5 w-[70px] h-[2.5px] bg-primary-500 rounded-full"></div>
+          <div className="absolute -bottom-2.5 w-[60px] h-[2.5px] bg-primary-500 rounded-full"></div>
         </div>
         {navigateElem}
       </div>
