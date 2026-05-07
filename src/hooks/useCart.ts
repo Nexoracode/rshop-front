d5afd96778317e8cad28200e37371717e70ac0d8
hosseinAmini <pershosien@gmail.com>
2026-05-07 12:26:10 +0330

    fix login button display bug

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import useCurrentUser from "./useCurrentUser";
 
 export default function useCart() {
-  const { user } = useCurrentUser();
-  const cart = useQuery(getCart(!!user));
+  const { isPending } = useCurrentUser();
+  const cart = useQuery(getCart(!isPending));
   return { ...cart };
 }
